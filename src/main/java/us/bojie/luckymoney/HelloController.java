c6d29c32bed233676db64ce764281cb465eefdac
Bojie Jiang <jiangbojie@gmail.com>
2019-04-24 23:35:01 -0700

    Request

--- src/main/java/us/bojie/luckymoney/HelloController.java
+++ src/main/java/us/bojie/luckymoney/HelloController.java
@@ -2,17 +2,21 @@ package us.bojie.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @Autowired
     private LimitConfig limitConfig;
 
-    @GetMapping("/hello")
-    public String say() {
-        return limitConfig.getDescription();
+    @GetMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+//        return limitConfig.getDescription();
+        return "id " + id;
     }
 }
